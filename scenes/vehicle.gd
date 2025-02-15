a204db23a9e7a6e322d1726bbf3ceb5d4f36b0ca
cryonixx <danielamalgapo0911@gmail.com>
2025-02-15 22:11:56 +0800

    sprite added (fire sprite, planet sprites)

--- scenes/vehicle.gd
+++ scenes/vehicle.gd
@@ -3,11 +3,13 @@ class_name Vehicle
 
 @export var initial_planet: Planet
 @export var rotation_speed = 90.0 * 2.0
+@export var fire: Sprite2D
 
 var force_multiplier: float = 30.0
 
 func _ready() -> void:
 	self.linear_velocity = initial_planet.initial_velocity
+	self.fire.hide()
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	var planets = get_tree().get_nodes_in_group("planet") as Array[Planet]
@@ -22,6 +24,10 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 		var rotated = self.rotation - PI / 2.0
 		var direction = Vector2(cos(rotated), sin(rotated))
 		force += direction * self.force_multiplier
+		self.fire.show()
+		
+	if Input.is_action_just_released("thrust"):
+		self.fire.hide()
 	
 	for planet in planets:
 		force += Simulation.calculate_force(
